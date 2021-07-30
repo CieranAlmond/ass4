8ebf7acf0fc6d4e6cf6c7eb22ab2a8735c9b2c03
CieranAlmond <cieran884@gmail.com>
2021-07-30 15:37:02 +0000

    initial commit

---
+++ run_kubernetes.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Step 1:
+dockerpath="kibnov/container:latest"
+
+# Step 2
+kubectl run kibnov \
+                --image=$dockerpath \
+                --port=1025
+
+# Step 3:
+sleep 5
+kubectl get pods
+
+# Step 4:
+kubectl port-forward kibnov 8000:1025
